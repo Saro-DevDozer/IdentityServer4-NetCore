80cf94c11534f82149d00bfbd4be7a0e5015c541
Saravanan <saravananchandrasekaran@kuyatechnologies.com>
2020-05-11 17:31:17 +0530

    basic setup added

---
+++ IdentityApi/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("Bearer").AddJwtBearer("Bearer", config =>
+            {
+                config.Authority = "http://localhost:5000/";
+                config.Audience = "IdentityApi";
+
+                config.RequireHttpsMetadata = false;
+            });
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ IdentityServer/Config.cs
@@ -0,0 +1,64 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+             {
+                 new ApiResource("IdentityApi"),
+             };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+         {
+             new IdentityResources.OpenId(),
+             new IdentityResources.Profile(),
+         };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        => new List<Client>
+         {
+             new Client
+             {
+                ClientId="client_id",
+                ClientSecrets ={new Secret("client_secret".ToSha256())},
+
+                 AllowedGrantTypes=GrantTypes.ClientCredentials,
+                 AllowedScopes={"IdentityApi" }
+             }
+         };
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+         {
+             new TestUser
+             {
+                 SubjectId = "1",
+                 Username = "james",
+                 Password = "password",
+                 Claims = new List<Claim>
+                 {
+                     new Claim("name", "James Bond"),
+                     new Claim("website", "https://james.com")
+                 }
+             }
+         };
+        }
+
+    }
+}
